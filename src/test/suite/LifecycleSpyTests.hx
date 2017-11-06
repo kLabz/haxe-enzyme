2bd23316c48471dd54f5104cbc2590bc0e42088b
Rudy Ges <rudy.geslin@gmail.com>
2017-11-06 21:50:54 +0100

    Added tests for lifecycle spies callbacks

--- src/test/suite/LifecycleSpyTests.hx
+++ src/test/suite/LifecycleSpyTests.hx
@@ -4,6 +4,7 @@ import buddy.SingleSuite;
 import jsdom.Jsdom;
 import enzyme.Enzyme.mount;
 import enzyme.LifecycleSpy;
+import enzyme.ReactWrapper;
 import react.ReactMacro.jsx;
 import test.component.TestClick;
 
@@ -64,6 +65,82 @@ class LifecycleSpyTests extends SingleSuite {
 				spy.componentWillReceiveProps.called.should.be(false);
 
 				spy.restore();
+			});
+
+			it("should call injected callbacks", {
+				var spy = new LifecycleSpy(TestClick);
+				var onClick = function(_) {};
+				var wrapper:ReactWrapper;
+
+				var methodCalls = {
+					render: 0,
+					onComponentDidMount: 0,
+					onComponentDidUpdate: 0,
+					onComponentWillMount: 0,
+					onComponentWillReceiveProps: 0,
+					onComponentWillUnmount: 0,
+					onComponentWillUpdate: 0,
+					onShouldComponentUpdate: 0
+				};
+
+				spy.onRender = function() {
+					switch (++methodCalls.render) {
+						case 1:
+							// Only render, componentWillMount and componentDidMount should be called
+							methodCalls.onComponentDidMount.should.be(1);
+							methodCalls.onComponentDidUpdate.should.be(0);
+							methodCalls.onComponentWillMount.should.be(1);
+							methodCalls.onComponentWillReceiveProps.should.be(0);
+							methodCalls.onComponentWillUnmount.should.be(0);
+							methodCalls.onComponentWillUpdate.should.be(0);
+							methodCalls.onShouldComponentUpdate.should.be(0);
+
+							// Simulate click to trigger setState & re-render
+							wrapper.find("button").simulate("click");
+
+						case 2:
+							methodCalls.onComponentDidMount.should.be(1);
+							methodCalls.onComponentDidUpdate.should.be(0);
+							methodCalls.onComponentWillMount.should.be(1);
+							methodCalls.onComponentWillReceiveProps.should.be(0);
+							methodCalls.onComponentWillUnmount.should.be(0);
+							methodCalls.onComponentWillUpdate.should.be(1);
+							methodCalls.onShouldComponentUpdate.should.be(1);
+
+							// Simulate click to trigger setState & re-render
+							wrapper.find("button").simulate("click");
+
+						case 3:
+							methodCalls.onComponentWillUpdate.should.be(2);
+							methodCalls.onShouldComponentUpdate.should.be(2);
+							methodCalls.onComponentDidUpdate.should.be(1);
+							spy.restore();
+
+						default:
+					}
+				};
+
+				spy.onComponentDidMount = function() methodCalls.onComponentDidMount++;
+				spy.onComponentDidUpdate = function(_, _) methodCalls.onComponentDidUpdate++;
+				spy.onComponentWillMount = function() methodCalls.onComponentWillMount++;
+				spy.onComponentWillReceiveProps = function(_) methodCalls.onComponentWillReceiveProps++;
+				spy.onComponentWillUnmount = function() methodCalls.onComponentWillUnmount++;
+				spy.onComponentWillUpdate = function(_, _) methodCalls.onComponentWillUpdate++;
+				spy.onShouldComponentUpdate = function(_, _) methodCalls.onShouldComponentUpdate++;
+
+				// No callback should have been called
+				methodCalls.render.should.be(0);
+				methodCalls.onComponentDidMount.should.be(0);
+				methodCalls.onComponentDidUpdate.should.be(0);
+				methodCalls.onComponentWillMount.should.be(0);
+				methodCalls.onComponentWillReceiveProps.should.be(0);
+				methodCalls.onComponentWillUnmount.should.be(0);
+				methodCalls.onComponentWillUpdate.should.be(0);
+				methodCalls.onShouldComponentUpdate.should.be(0);
+
+				wrapper = mount(jsx('
+					<$TestClick onClick=$onClick />
+				'));
 			});
 		});
 	}
